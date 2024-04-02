e22a72984305b74480559ce950ff87e1d7da686e
Rhianna Heflin <rhiannaheflin43@gmail.com>
2024-04-02 12:57:57 -0400

    Added a list of puppy names to screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
+import {useState} from 'react'
+import {puppyList} from './data.js'
 
 function App() {
 
+const [puppies, setPuppies] = useState(puppyList);
 
+console.log("pupptList:", puppyList);
   return (
     <>
-     
+     {
+      puppies.map((puppy) => {
+        return <p key = {puppy.id}>{puppy.name}</p>;
+      })
+     }
     </>
-  )
+  );
 }
 
 export default App
