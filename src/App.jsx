46e62f9257ca79f142a9490a0c0d3092362460be
Rhianna Heflin <rhiannaheflin43@gmail.com>
2024-04-03 12:13:07 -0400

    updated list of puppy names to display details of a selected puppy

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
-import {useState} from 'react'
-import {puppyList} from './data.js'
+import {useState} from 'react';
+import {puppyList} from './data.js';
+import './index.css';
+import './App.css';
 
 function App() {
 
 const [puppies, setPuppies] = useState(puppyList);
-
+const [featPupId, setFeatPupId] = useState(null);
 console.log("pupptList:", puppyList);
-  return (
+const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+console.log(featuredPup);
+
+  return ( 
     <>
      {
       puppies.map((puppy) => {
-        return <p key = {puppy.id}>{puppy.name}</p>;
-      })
-     }
+     return <p onClick = {()=>{setFeatPupId(puppy.id)}} key = {puppy.id}>{puppy.name}</p>;
+      }) 
+    }
+
+    {featPupId && (
+      <div>
+        <h2 className = "Selected-pup">{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+    )}
     </>
+   
   );
 }
 
